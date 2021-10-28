d24268d68b91a215781b567dc32bc23b20efad1e
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-10-28 17:03:14 +0300

    Add ArrayList implementation with static size

--- src/main/java/com/luxoft/datastructures/list/ArrayList.java
+++ src/main/java/com/luxoft/datastructures/list/ArrayList.java
@@ -1,58 +1,142 @@
 package com.luxoft.datastructures.list;
 
 public class ArrayList implements List{
+    private int head;
+    private Object[] arrayList;
+
+    public ArrayList() {
+        arrayList = new Object[10000];
+    }
+
     @Override
     public void add(Object value) {
-
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
+        arrayList[head++] = value;
     }
 
     @Override
     public void add(Object value, int index) {
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
 
+        if(index < 0 || index >= head) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bound");
+        }
+
+        for (int i = head; i > index; --i) {
+            arrayList[i] = arrayList[i - 1];
+        }
+
+        arrayList[index] = value;
     }
 
     @Override
     public Object remove(int index) {
-        return null;
+        if(index < 0 || index >= head) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bound");
+        }
+
+        Object result = arrayList[index];
+
+        for (int i = index; i < head - 1; ++i) {
+            arrayList[i] = arrayList[i + 1];
+        }
+
+        arrayList[--head] = null;
+
+        return result;
     }
 
     @Override
     public Object get(int index) {
-        return null;
+        if(index < 0 || index >= head) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bound");
+        }
+        return arrayList[index];
     }
 
     @Override
     public Object set(Object value, int index) {
-        return null;
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
+
+        if(index < 0 || index >= head) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bound");
+        }
+
+        Object result = arrayList[index];
+        arrayList[index] = value;
+        return result;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < head; i++) {
+            arrayList[i] = null;
+        }
+        head = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return head;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return head == 0;
     }
 
     @Override
     public boolean contains(Object value) {
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
+
+        for (int i = 0; i < head; i++) {
+            if(arrayList[i].equals(value)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     @Override
     public int indexOf(Object value) {
-        return 0;
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
+
+        for (int i = 0; i < head; i++) {
+            if(arrayList[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object value) {
-        return 0;
+        if(value == null) {
+            throw new NullPointerException("Null values are not support");
+        }
+
+        if(isEmpty()) {
+            return -1;
+        }
+
+        for (int i = head - 1; i >= 0; --i) {
+            if(arrayList[i].equals(value)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
--- src/test/java/com/luxoft/datastructures/list/ArrayListTest.java
+++ src/test/java/com/luxoft/datastructures/list/ArrayListTest.java
@@ -1,4 +1,393 @@
 package com.luxoft.datastructures.list;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayListTest {
+    @Test
+    public void testAddToListAndSize() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Apple");
+        arrayList.add("Banana");
+        arrayList.add("Pizza");
+        arrayList.add("Cake");
+
+        assertEquals(4, arrayList.size());
+    }
+
+    @Test
+    public void testAddToListNull() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.add(null));
+    }
+
+    @Test
+    public void testAddToListHugeAmountOfItemsAndSize() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 1_000_000_000; i++) {
+            arrayList.add(i);
+        }
+
+        assertEquals(1_000_000_000, arrayList.size());
+    }
+
+    @Test
+    public void testAddAndGetItems() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Pizza");
+        arrayList.add("Burger");
+        arrayList.add("Cheese");
+
+        assertEquals("Cheese", arrayList.get(2));
+        assertEquals("Pizza", arrayList.get(0));
+        assertEquals("Burger", arrayList.get(1));
+    }
+
+    @Test
+    public void testAddAndGetWithIncorrectIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add(2.4);
+        arrayList.add(0.23);
+        arrayList.add(-12.4);
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(-1));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> arrayList.get(3));
+    }
+
+    @Test
+    public void testSetItems() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Apple");
+        arrayList.add("Cola");
+
+        arrayList.set("Cheese", 1);
+        arrayList.set("Pasta", 0);
+
+        assertEquals("Cheese", arrayList.get(1));
+        assertEquals("Pasta", arrayList.get(0));
+    }
+
+    @Test
+    public void testSetItemsWithIncorrectIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Apple");
+        arrayList.add("Cola");
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.set("Hello", -1));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.set("Error!!!", 2));
+    }
+
+    @Test
+    public void testSetNullItem() {
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.add("Hello");
+
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.set(null, 0));
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.set(null, 1));
+    }
+
+    @Test
+    public void testSetToEmptyList() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.set("Yo", 0));
+    }
+
+    @Test
+    public void testGetFromEmptyList() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.get(0));
+    }
+
+    @Test
+    public void testAddByIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        arrayList.add(100, 4);
+        assertEquals(100, arrayList.get(4));
+    }
+
+    @Test
+    public void testAddByFirstIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        arrayList.add(100, 0);
+        assertEquals(100, arrayList.get(0));
+    }
+
+    @Test
+    public void testAddByLastIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        arrayList.add(100, 9);
+        assertEquals(100, arrayList.get(9));
+    }
+
+    @Test
+    public void testAddNByIncorrectIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.add(102, -1));
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.add(102, 10));
+    }
+
+    @Test
+    public void testAddNullValueByIndex() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Hi");
+
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.add(null, 0));
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.add(null, 1));
+    }
+
+    @Test
+    public void testSizeWhenListIsEmpty() {
+        ArrayList arrayList = new ArrayList();
+        assertEquals(0, arrayList.size());
+    }
+
+    @Test
+    public void testIsEmptyWhenListContainsItems() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        assertEquals(false, arrayList.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyWhenListNotContainsItems() {
+        ArrayList arrayList = new ArrayList();
+        assertEquals(true, arrayList.isEmpty());
+    }
+
+    @Test
+    public void testClearAndSizeAndAdd() {
+        ArrayList arrayList = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+        assertEquals(10, arrayList.size());
+        arrayList.clear();
+        assertEquals(0, arrayList.size());
+        arrayList.clear();
+        assertEquals(0, arrayList.size());
+    }
+
+    @Test
+    public void testClearAndSizeWhenListIsEmpty() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.clear();
+        assertEquals(0, arrayList.size());
+    }
+
+    @Test
+    public void testRemove() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Vine");
+        arrayList.add("Papaya");
+
+        arrayList.remove(2);
+        assertEquals(3, arrayList.size());
+
+        assertEquals("Apple", arrayList.get(0));
+        assertEquals("Melon", arrayList.get(1));
+        assertEquals("Papaya", arrayList.get(2));
+    }
+
+    @Test
+    public void testRemoveFirstItem() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Vine");
+        arrayList.add("Papaya");
+
+        arrayList.remove(0);
+        assertEquals(3, arrayList.size());
+
+        assertEquals("Melon", arrayList.get(0));
+        assertEquals("Vine", arrayList.get(1));
+        assertEquals("Papaya", arrayList.get(2));
+    }
+
+    @Test
+    public void testRemoveLastItem() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Vine");
+        arrayList.add("Papaya");
+
+        arrayList.remove(3);
+        assertEquals(3, arrayList.size());
+
+        assertEquals("Apple", arrayList.get(0));
+        assertEquals("Melon", arrayList.get(1));
+        assertEquals("Vine", arrayList.get(2));
+    }
+
+    @Test
+    public void testRemoveManyItems() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(i);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            assertEquals(1000 - i, arrayList.size());
+            assertEquals( i, arrayList.remove(0));
+        }
+
+        assertEquals(0, arrayList.size());
+    }
+
+    @Test
+    public void testRemoveWhenListEmpty() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.remove(0));
+    }
+
+    @Test
+    public void testRemoveWithIncorrectIndex() {
+        ArrayList arrayList = new ArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(i);
+        }
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.remove(-1));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+                arrayList.remove(10));
+    }
+
+    @Test
+    public void testContains() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Vine");
+        arrayList.add("Papaya");
+
+        assertEquals(true, arrayList.contains("Apple"));
+        assertEquals(true, arrayList.contains("Melon"));
+        assertEquals(true, arrayList.contains("Vine"));
+        assertEquals(true, arrayList.contains("Papaya"));
+
+        assertEquals(false, arrayList.contains("Pizza"));
+        assertEquals(false, arrayList.contains(-1));
+    }
+
+    @Test
+    public void testContainsWhenListIsEmpty() {
+        ArrayList arrayList = new ArrayList();
+        assertEquals(false, arrayList.contains("Pizza"));
+    }
+
+    @Test
+    public void testContainsWhenSearchedValueIsNull() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.contains(null));
+    }
+
+    @Test
+    public void testIndexOf() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Melon");
+        arrayList.add("Papaya");
+
+        assertEquals(0, arrayList.indexOf("Apple"));
+        assertEquals(1, arrayList.indexOf("Melon"));
+        assertEquals(1, arrayList.indexOf("Melon"));
+        assertEquals(3, arrayList.indexOf("Papaya"));
+
+        assertEquals(-1, arrayList.indexOf("Pizza"));
+        assertEquals(-1, arrayList.indexOf("Paprika"));
+    }
+
+    @Test
+    public void testIndexOfWhenSeachedValueIsNull() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.indexOf(null));
+    }
+
+    @Test
+    public void testIndexOfWhenListIsEmpty() {
+        ArrayList arrayList = new ArrayList();
+        assertEquals(-1, arrayList.indexOf("Hi"));
+    }
+
+    @Test
+    public void testLastIndexOf() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Apple");
+        arrayList.add("Melon");
+        arrayList.add("Melon");
+        arrayList.add("Papaya");
+
+        assertEquals(0, arrayList.lastIndexOf("Apple"));
+        assertEquals(2, arrayList.lastIndexOf("Melon"));
+        assertEquals(2, arrayList.lastIndexOf("Melon"));
+        assertEquals(3, arrayList.lastIndexOf("Papaya"));
+
+        assertEquals(-1, arrayList.lastIndexOf("Pizza"));
+        assertEquals(-1, arrayList.lastIndexOf("Paprika"));
+    }
+
+    @Test
+    public void testLastIndexOfWhenSeachedValueIsNull() {
+        ArrayList arrayList = new ArrayList();
+        Assertions.assertThrows(NullPointerException.class, () ->
+                arrayList.lastIndexOf(null));
+    }
+
+    @Test
+    public void testLastIndexOfWhenListIsEmpty() {
+        ArrayList arrayList = new ArrayList();
+        assertEquals(-1, arrayList.lastIndexOf("Hi"));
+    }
+
 }
